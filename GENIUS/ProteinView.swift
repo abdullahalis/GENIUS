37108d8d4cf71026d61c8c2f98bb732942bea63c
aaqelshaik <mshaikabdulmazeed@anl.gov>
2024-05-23 10:32:38 -0500

    Added ProteinView

---
+++ GENIUS/ProteinView.swift
@@ -0,0 +1,46 @@
+//
+//  ProteinView.swift
+//  GENIUS
+//
+//  Created by Aaqel Shaik on 5/23/24.
+//
+
+import SwiftUI
+
+struct ProteinView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                VStack {
+                    proteinMenuItems()
+                    VStack {
+                        NavigationLink("Go back", destination: ContentView())
+                            .padding()
+                    }
+                    .navigationTitle("Protein View")
+                }
+            }
+        }
+    }
+}
+
+struct proteinMenuItems: View {
+    var body: some View {
+        
+        VStack {
+            Text("Welcome to the ProteinView")
+                .font(.system(size: 30, weight: .medium))
+            Image(systemName: "lizard.circle")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90, height: 90)
+        }
+        .padding(.bottom, 40)
+
+    }
+}
+
+#Preview {
+    ProteinView()
+}
