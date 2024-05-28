cfaaec46b4f587248dd6af31889420b5ff56451a
aaqelshaik <mshaikabdulmazeed@anl.gov>
2024-05-28 15:40:25 -0500

    Removed boilerplate code

--- GENIUS/Protein.swift
+++ GENIUS/Protein.swift
@@ -6,26 +6,6 @@
 //
 import Foundation
 
-// Get string of proteins, map them.
-// Use the mapped identifiers to request interactions data
-let method = "network"
-
-
-//let myGenes = ["CDC42","CDK1","KIF23","PLK1",
-//              "RAC2","RACGAP1","RHOA","RHOB"]
-
-//let identifiers = myGenes.joined(separator: "%0d")
-let identifiers = "CDC42"
-let species = "9606" // species NCBI identifier
-let callerIdentity = "Genius" // your app name
-
-let params = [
-    "identifiers": identifiers,
-    "species": species,
-    "caller_identity": callerIdentity
-]
-
-
 // General function to make API requests
 // Returns a string with requested data
 func requestAPI(method: String, params: [String:Any], completion: @escaping (String?) -> Void) {
