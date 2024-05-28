247746bef2cc180348d92bf9c2874aa37d824430
aaqelshaik <mshaikabdulmazeed@anl.gov>
2024-05-28 14:49:11 -0500

    Added functionality to fetch network data

--- GENIUS/Protein.swift
+++ GENIUS/Protein.swift
@@ -28,19 +28,19 @@ let params = [
 
 // General function to make API requests
 // Returns a string with requested data
-func requestAPI(method: String, params: [String:String]) -> String? {
+func requestAPI(method: String, params: [String:Any], completion: @escaping (String?) -> Void) {
     
     let stringAPIURL = "https://version-11-5.string-db.org/api"
     let outputFormat = "tsv-no-header"
     let requestURL = "\(stringAPIURL)/\(outputFormat)/\(method)"
     var stringResponse: String?
     let session = URLSession.shared
-    
+
     if let url = URL(string: requestURL) {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        
+
         let requestBody = params.map { (key, value) in
             return "\(key)=\(value)"
         }.joined(separator: "&")
@@ -49,28 +49,78 @@ func requestAPI(method: String, params: [String:String]) -> String? {
         let task = session.dataTask(with: request) { data, response, error in
             if let error = error {
                 print("Error: \(error)")
+                completion(nil)
                 return
             }
-            if let data = data {stringResponse = String(data: data, encoding: .utf8)}
+            if let data = data {
+                stringResponse = String(data: data, encoding: .utf8)
+                completion(stringResponse)
+            }
         }
         task.resume()
-    } else {print("Invalid URL")}
-    
-    return stringResponse
+    } else {
+        print("Invalid URL")
+        completion(nil)
+    }
 }
 
 // Function to get proteins
-func getProteins() {
-    let lines = stringResponse.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")
-    
-    for line in lines {
-        let components = line.components(separatedBy: "\t")
-        if components.count >= 11 {
-            let p1 = components[2]
-            let p2 = components[3]
-            if let experimentalScore = Double(components[10]), experimentalScore > 0.4 {
-                print("\(p1)\t\(p2)\texperimentally confirmed (prob. \(String(format: "%.3f", experimentalScore)))")
+func getProteins(proteins: [String]) {
+    mapIdentifiers(proteinNames: proteins) { identifiers in
+
+        if !identifiers.isEmpty {
+            let params: [String: Any] = [
+                "identifiers": Array(identifiers.keys).joined(separator: "%0d"),
+                "species": 9606,
+                "caller_identity": "Genius"
+            ]
+            
+            requestAPI(method: "network", params: params) { response in
+                if let proteinString = response {
+                    let lines = proteinString.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")
+                    for line in lines {
+                        let components = line.components(separatedBy: "\t")
+                        if components.count >= 11 {
+                            let p1 = components[2]
+                            let p2 = components[3]
+                            if let experimentalScore = Double(components[10]), experimentalScore > 0.4 {
+                                print("\(p1)\t\(p2)\texperimentally confirmed (prob. \(String(format: "%.3f", experimentalScore)))")
+                            }
+                        }
+                    }
+                } else {print("The database returned NULL.")}
             }
+        } else {print("Protein(s) couldn't be mapped to valid identifiers.")}
+    }
+}
+
+// Function to map user-submitted protein names to STRING identifiers
+// [Identifier:Name]
+func mapIdentifiers(proteinNames: [String], completion: @escaping ([String:String]) -> Void) {
+    var mapDict: [String:String] = [:]
+    let params: [String: Any] = [
+        "identifiers": proteinNames.joined(separator: "\r"),
+        "species": 9606,
+        "caller_identity": "Genius"
+    ]
+    
+    requestAPI(method: "get_string_ids", params: params) { response in
+        if let proteinString = response {
+
+            let lines = proteinString.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")
+            for line in lines {
+                let components = line.components(separatedBy: "\t")
+                if components.count >= 5 {
+                    let name = components[4]
+                    let identifier = components[1]
+                    print("\(identifier): \(name)")
+                    mapDict[identifier] = name
+                }
+            }
+            completion(mapDict)
+        } else {
+            print("The database returned NULL.")
+            completion([:])
         }
     }
 }
