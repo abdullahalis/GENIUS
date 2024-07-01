ab54f1367eed6ba78d005c8d809c6aca9e40e8e3
Fredric Massa <rickmass2020@gmail.com>
2024-07-01 10:26:40 -0500

    sim now working

---
+++ GENIUS/Views/SimulationsView.swift
@@ -0,0 +1,18 @@
+//
+//  SimulationsView.swift
+//  GENIUS
+//
+//  Created by Rick Massa on 6/24/24.
+//
+
+import SwiftUI
+
+struct SimulationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimulationsView()
+}
